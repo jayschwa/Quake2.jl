57a7cac46591b12eef45a19f85ba0bcc729c5edc
Jay Weisskopf <jay@jayschwa.net>
2013-03-05 20:54:29 -0500

    cube: Rotating camera (spaghetti code)

--- test/cube.jl
+++ test/cube.jl
@@ -70,10 +70,8 @@ const indices = Uint8[
 	1, 7, 5,
 ]
 
-modelMatrix = float32(eye(4))
-
 const near = 1
-const far = 10
+const far = 1000
 projMatrix = float32(eye(4))
 fov = 90.0
 
@@ -84,6 +82,8 @@ function translationMatrix(x::Number, y::Number, z::Number)
 	T[15] = float32(z)
 	return T
 end
+
+modelMatrix = translationMatrix(0, 0, 0)
 
 function updateProjMatrix(width::Cint, height::Cint)
 	GL.Viewport(width, height)
@@ -117,6 +117,7 @@ GLFW.OpenWindowHint(GLFW.OPENGL_FORWARD_COMPAT, 1)
 GLFW.OpenWindow(0, 0, 8, 8, 8, 8, 16, 0, GLFW.WINDOW)
 GLFW.SetWindowTitle("GL for Julia")
 GLFW.SetWindowSizeCallback(updateProjMatrix)
+GLFW.SwapInterval(0)
 
 GL.Enable(GL.CULL_FACE)
 GL.Enable(GL.DEPTH_TEST)
@@ -169,15 +170,13 @@ tic()
 tic()
 
 cam_speed = 0.5 # unit/sec
-cam_x = 0
-cam_y = 0
-cam_z = 0
+cam_pos = float32([-3, 0, 0])
 
 m_captured = false
 function m_capture(capture::Bool)
 	if capture
 		GLFW.Disable(GLFW.MOUSE_CURSOR)
-		GLFW.SetMousePos(0, 0)
+		#GLFW.SetMousePos(0, 0)
 		global m_captured = true
 	else
 		GLFW.Enable(GLFW.MOUSE_CURSOR)
@@ -186,27 +185,74 @@ function m_capture(capture::Bool)
 end
 m_capture() = m_captured
 
+const m_pitch = 0.05
+const m_yaw = 0.05
+
+function sphereToCartesian(yaw::Number, pitch::Number)
+	# apply scalar and convert to radians
+	yaw = m_yaw * yaw * (pi / 180)
+	pitch = m_pitch * pitch * (pi / 180)
+
+	x = cos(yaw)*cos(pitch)
+	y = sin(yaw)*cos(pitch)
+	z = sin(pitch)
+	return Float32[x, y, z]
+end
+
+function rotationMatrix(eyeDir::Vector, upDir::Vector)
+	rightDir = cross(eyeDir, upDir)
+	rightDir /= norm(rightDir)
+	upDir = cross(rightDir, eyeDir)
+
+	rotMat = float32(eye(4))
+
+	rotMat[1] = rightDir[1]
+	rotMat[5] = rightDir[2]
+	rotMat[9] = rightDir[3]
+
+	rotMat[2] = upDir[1]
+	rotMat[6] = upDir[2]
+	rotMat[10] = upDir[3]
+
+	rotMat[3] = -eyeDir[1]
+	rotMat[7] = -eyeDir[2]
+	rotMat[11] = -eyeDir[3]
+
+	return rotMat
+end
+
 while GLFW.GetWindowParam(GLFW.OPENED)
+	if m_capture()
+		m_pos = GLFW.GetMousePos()
+		m_pos = (-m_pos[1], -m_pos[2])
+		eyeDir = sphereToCartesian(m_pos...)
+	else
+		eyeDir = sphereToCartesian(0, 0)
+	end
+	rightDir = cross(eyeDir, Float32[0, 0, 1])
+	rightDir /= norm(rightDir)
+	rotMat = rotationMatrix(eyeDir, float32([0, 0, 1]))
 	dist = cam_speed * toq()
 	if m_capture()
 		if GLFW.GetKey(',')
-			cam_z -= dist
+			cam_pos += dist * eyeDir
 		end
 		if GLFW.GetKey('A')
-			cam_x -= dist
+			cam_pos -= dist * rightDir
 		end
 		if GLFW.GetKey('O')
-			cam_z += dist
+			cam_pos -= dist * eyeDir
 		end
 		if GLFW.GetKey('E')
-			cam_x += dist
+			cam_pos += dist * rightDir
 		end
 		if GLFW.GetKey(' ')
-			cam_y += dist
+			cam_pos[3] += dist
 		end
 		if GLFW.GetKey(GLFW.KEY_LCTRL)
-			cam_y -= dist
+			cam_pos[3] -= dist
 		end
+		#GLFW.SetMousePos(0, 0)
 	end
 	if GLFW.GetMouseButton(GLFW.MOUSE_BUTTON_LEFT)
 		m_capture(true)
@@ -219,7 +265,9 @@ while GLFW.GetWindowParam(GLFW.OPENED)
 	GL.Clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT)
 	GL.UseProgram(prog)
 	GL.UniformMatrix4fv(uModel, modelMatrix)
-	GL.UniformMatrix4fv(uView, translationMatrix(-cam_x, -cam_y, -cam_z))
+	transMat = translationMatrix(-cam_pos[1], -cam_pos[2], -cam_pos[3])
+	viewMat = rotMat * transMat
+	GL.UniformMatrix4fv(uView, viewMat)
 	GL.UniformMatrix4fv(uProj, projMatrix)
 	GL.BindVertexArray(vao)
 	GL.DrawElements(GL.TRIANGLES, indices)
