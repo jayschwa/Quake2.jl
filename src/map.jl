fd156aacf9df94552dba4909e2932869b10d12da
Jay Weisskopf <jay@jayschwa.net>
2013-04-09 17:58:44 -0400

    Light: Remove goofy min distance for specular

--- src/map.jl
+++ src/map.jl
@@ -149,7 +149,7 @@ void main()
 			LightColor += Light[i].Color * dirMod * distMod;
 		}
 		if (SpecularLighting) {
-			LightColor += Light[i].Color * pow(max(dot(lightDir, camReflectDir), 0.0), 10) * distMod * min(pow(1.5 * lightDist / Light[i].Power, 3), 1);
+			LightColor += Light[i].Color * pow(max(dot(lightDir, camReflectDir), 0.0), 10) * distMod * 0.8;
 		}
 	}
 	LightColor = min(LightColor, vec3(1.0, 1.0, 1.0));
