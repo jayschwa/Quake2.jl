747fdd3abd1f6cb438ddbb68e08389dd773bcf39
Jay Weisskopf <jay@jayschwa.net>
2013-06-13 13:59:35 -0400

    Add UVs

--- src/threejs.jl
+++ src/threejs.jl
@@ -12,6 +12,13 @@ function vecstr(v::AbstractVector)
 	s = replace(s, "f0", "")
 end
 
+function calcuv(face, point)
+	point = cat(1, point, float32(1))
+	u = dot(face.u_axis, point) / face.texture.width
+	v = dot(face.v_axis, point) / face.texture.height
+	return string("new THREE.Vector2(", vecstr([u, v]), ")")
+end
+
 js = open(string(ARGS[1], ".js"), "w")
 
 write(js, "var geometry = new THREE.Geometry();\n")
@@ -23,7 +30,12 @@ for f = bsp.faces
 	if length(f.indices) % 3 == 0
 		i = 1
 		while i < length(f.indices)
-			write(js, "geometry.faces.push(new THREE.Face3("*vecstr(f.indices[i:i+2])*"));\n")
+			vidxs = f.indices[i:i+2]
+			write(js, "geometry.faces.push(new THREE.Face3("*vecstr(vidxs)*"));\n")
+			uv1 = calcuv(f, bsp.vertices[vidxs[1]+1])
+			uv2 = calcuv(f, bsp.vertices[vidxs[2]+1])
+			uv3 = calcuv(f, bsp.vertices[vidxs[3]+1])
+			write(js, string("geometry.faceVertexUvs[0].push([", uv1, ",", uv2, ",", uv3, "]);\n"))
 			i += 3
 		end
 	else
